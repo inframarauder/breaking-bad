62563e16d0865cad422781f8b71af4bb0dadcc16
subhasis020299 <subhasisdas125@gmail.com>
2020-07-07 02:13:59 +0530

    quotes carousel done

--- src/Components/CharacterCard.js
+++ src/Components/CharacterCard.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
-import { Card } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Card, Carousel } from "react-bootstrap";
 
 const CharacterCard = ({ character }) => {
   const [opacity, setOpacity] = useState("opacity-0");
+  const [quotes, setQuotes] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get("https://breakingbadapi.com/api/quote", {
+          params: { author: character.name },
+        });
+        setQuotes(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
 
   const toggleOpacity = () => {
     setOpacity((opacity) =>
       opacity === "opacity-0" ? "opacity-08" : "opacity-0"
     );
   };
+
   return (
     <Card className="character-card">
       <Card.Img variant="top" src={character.img} className="character-img" />
@@ -17,13 +33,13 @@ const CharacterCard = ({ character }) => {
         onMouseOver={toggleOpacity}
         onMouseOut={toggleOpacity}
       >
-        <div className="character-details-header">
+        <div className="character-details">
           <h4>
             {character.name}({character.nickname})
           </h4>
           <p>
-            {character.occupation.map((occupation) => (
-              <span>
+            {character.occupation.map((occupation, index) => (
+              <span key={index}>
                 {occupation}
                 <br />
               </span>
@@ -34,8 +50,20 @@ const CharacterCard = ({ character }) => {
             {character.status}
           </p>
         </div>
-
         <hr />
+        <div className="quotes">
+          {quotes.length > 0 ? (
+            <Carousel indicators={false} controls={false}>
+              {quotes.map((quote) => (
+                <Carousel.Item key={quote.quote_id}>
+                  <p>{quote.quote}</p>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          ) : (
+            "Loading.."
+          )}
+        </div>
       </div>
     </Card>
   );
