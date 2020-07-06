eeed292a4660875baa9b928f944d62cb70c13956
subhasis020299 <subhasisdas125@gmail.com>
2020-07-06 22:23:06 +0530

    character details displayed

--- src/Components/CharacterCard.js
+++ src/Components/CharacterCard.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 
 const CharacterCard = ({ character }) => {
+  const [opacity, setOpacity] = useState("opacity-0");
+
+  const toggleOpacity = () => {
+    setOpacity((opacity) =>
+      opacity === "opacity-0" ? "opacity-08" : "opacity-0"
+    );
+  };
   return (
     <Card className="character-card">
       <Card.Img variant="top" src={character.img} className="character-img" />
-      <Card.Body>
-        <Card.Title>{character.name}</Card.Title>
-      </Card.Body>
+      <div
+        className={`middle ${opacity}`}
+        onMouseOver={toggleOpacity}
+        onMouseOut={toggleOpacity}
+      >
+        <div className="character-details-header">
+          <h4>
+            {character.name}({character.nickname})
+          </h4>
+          <p>
+            {character.occupation.map((occupation) => (
+              <span>
+                {occupation}
+                <br />
+              </span>
+            ))}
+            <br />
+            Born : {character.birthday}
+            <br />
+            {character.status}
+          </p>
+        </div>
+
+        <hr />
+      </div>
     </Card>
   );
 };
