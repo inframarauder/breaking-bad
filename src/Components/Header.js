a965eaa7b461f76407197db9b799352651f417fe
subhasis020299 <subhasisdas125@gmail.com>
2020-07-06 20:58:31 +0530

    theme toggler complete

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,9 +6,13 @@ const Header = () => {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <div className={`header ${theme.isDarkTheme ? theme.dark : theme.light}`}>
-      <img className="logo" src="/assets/logo.png" alt="logo" />
-      <ThemeToggler />
+    <div className={`header  ${theme.isDarkTheme ? theme.dark : theme.light}`}>
+      <div className="theme-toggler">
+        <ThemeToggler />
+      </div>
+      <div className="logo-container">
+        <img className="logo" src="/assets/logo.png" alt="logo" />
+      </div>
     </div>
   );
 };
